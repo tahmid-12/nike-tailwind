79e3d1f08bfed9e2acd5c058bd3249e7638d3ebc
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2023-08-31 20:35:50 +0600

    Files and images added'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        Hero
+      </section>
+      <section className="padding">
+        Popular Products
+      </section>
+      <section className="padding">
+        Super Quality
+      </section>
+      <section className="padding-x py-10">
+        Services
+      </section>
+      <section className="padding">
+        Special Offers
+      </section>
+      <section className="bg-pale-blue padding">
+        Customer Reviews
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        Subscribe
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        Footer
+      </section>
+    </main>
   )
 }
