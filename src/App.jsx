804b88cab9dcf24d165f593abaccfb2f7b248556
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2023-08-31 22:18:56 +0600

    Files created and placed on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
+import { CustomerReview, Footer, Hero, Navbar, PopularProducts, Services, SpecialOffers, Subscribe, SuperQuality } from "./components";
+
+
 export default function App() {
   return (
     <main className="relative">
-      Nav
+      <Navbar />
       <section className="xl:padding-1 wide:padding-r padding-b">
-        Hero
+        <Hero />
       </section>
       <section className="padding">
-        Popular Products
+        <PopularProducts />
       </section>
       <section className="padding">
-        Super Quality
+        <SuperQuality />
       </section>
       <section className="padding-x py-10">
-        Services
+        <Services />
       </section>
       <section className="padding">
-        Special Offers
+        <SpecialOffers />
       </section>
       <section className="bg-pale-blue padding">
-        Customer Reviews
+        <CustomerReview />
       </section>
       <section className="padding-x sm:py-32 py-16 w-full">
-        Subscribe
+        <Subscribe />
       </section>
       <section className="bg-black padding-x padding-t pb-8">
-        Footer
+        <Footer />
       </section>
     </main>
   )
---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers';
+import CustomerReview from './CustomerReview';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export {
+    Navbar,
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffers,
+    CustomerReview,
+    Subscribe,
+    Footer
+}
