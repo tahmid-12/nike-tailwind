85c01c906e74089e1ea8ff939ada5ef3cd490ddd
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2023-09-02 19:28:54 +0600

    Popular product Section

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
 import Button from "./Button";
 import ShoeCard from './ShoeCard';
+import PopularProductCard from "./PopularProductCard";
 
 export {
     Navbar,
     Button,
-    ShoeCard
+    ShoeCard,
+    PopularProductCard
 }
