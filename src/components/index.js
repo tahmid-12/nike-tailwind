c9d89fd304b208c3d25d211a0de8d6ab4ee375f9
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2023-09-02 20:31:42 +0600

    Services Section

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Navbar from "./Navbar";
 import Button from "./Button";
 import ShoeCard from './ShoeCard';
 import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard ";
 
 export {
     Navbar,
     Button,
     ShoeCard,
-    PopularProductCard
+    PopularProductCard,
+    ServiceCard
 }
